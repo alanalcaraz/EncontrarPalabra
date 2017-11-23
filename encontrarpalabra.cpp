64275dac71ed111db9ae00351c6eab653a5ba1e6
alanalcaraz <31902909+alanalcaraz@users.noreply.github.com>
2017-11-23 15:32:51 -0600

    Add files via upload

---
+++ encontrarpalabra.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int encontrarPalabra(char palabra[], char nombre[], int posicion, int contador, int c);
+
+int main(void) 
+{
+    char nombre[50];
+    char palabra[30];
+    int contador=0;
+    int c=0;
+    int posicion=0;
+    cout<<"Introducir texto: ";
+    cin.getline(nombre, 50, '\n');
+    cout<<"Ingrese la palabra a buscar: ";
+    cin.getline(palabra, 30, '\n');
+    encontrarPalabra (palabra, nombre, posicion, contador, c);
+}
+
+int encontrarPalabra(char palabra[], char nombre[], int posicion, int contador, int c)
+{
+	for (int i=0; nombre[i]!='\0'; i++){
+        if (palabra[0]==nombre[i]){
+            posicion = i;
+            for (contador=0; palabra[contador]!='\0'; contador++,i++) {
+                if (palabra[contador]==nombre[i])
+                c=-1;
+                else
+                    c=0;
+            }
+        if (c!=0)
+           break;
+        }
+    }
+    if (c!=0)
+        cout<<"La palabra fue encontrada en la posicion "<<posicion+1;
+    else
+        cout<<"La palabra no fue encontrada";
+}
